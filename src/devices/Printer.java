0499ea26dab7eaec1551e8e7987598f43d0b9293
João Pedro <jpsdb10@hotmail.com>
2021-06-22 16:17:45 -0300

    solucionando o problema do diamante tirando o reuso entre as classes utilizando interfaces

--- src/devices/Printer.java
+++ src/devices/Printer.java
@@ -1,17 +1,7 @@
 package devices;
 
-public class Printer extends Device {
-
-	public Printer(String serialNumber) {
-		super(serialNumber);
-	}
-
-	@Override
-	public void processDoc(String doc) {
-		System.out.println("Printer processing: " + doc);
-	}
+public interface Printer {
 	
-	public void print(String doc) {
-		System.out.println("Printing: " + doc);
-	}
+	void print(String doc);
+
 }
--- src/devices/Scanner.java
+++ src/devices/Scanner.java
@@ -1,18 +1,7 @@
 package devices;
 
-public class Scanner extends Device {
-
-	public Scanner(String serialNumber) {
-		super(serialNumber);
-	}
-
-	@Override
-	public void processDoc(String doc) {
-		System.out.println("Scanner processing: " + doc);
-	}
+public interface Scanner {
 	
-	public String scan() {
-		return "Scanned content";
-	}
+	String scan();
 
 }
